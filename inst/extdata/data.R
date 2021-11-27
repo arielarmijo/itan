b21e3bef305481afe3e01938b7d6892d47c01bde
Ariel Armijo <arielarmijo@yahoo.es>
2021-11-27 20:14:28 -0300

    Agrega vignette y documentación del paquete.

--- inst/extdata/data.R
+++ inst/extdata/data.R
@@ -1,3 +1,3 @@
-datos <- read.csv("inst/extdata/datos.csv", na.strings = "*")
+datos <- read.csv("inst/extdata/datos.csv")
 clave <- read.csv("inst/extdata/clave.csv")
 usethis::use_data(datos, clave, overwrite = TRUE)
